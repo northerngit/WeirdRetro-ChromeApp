90b796f19320d4a4dbbff71368c850f94e7e0343
James Wilson <jbw@me.com>
2015-08-02 14:23:14 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Weird Retro",
+   "homepage": "http://www.weirdretro.org.uk",
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
